90534a44a2c22b66afbc448905c5be3fa9ad37ef
Toshin Prince <toshinprince@gmail.com>
2023-07-04 10:54:22 +0530

    Contract List

--- contracts/EtherZon.sol
+++ contracts/EtherZon.sol
@@ -1,4 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract EtherZon {}
+contract EtherZon {
+    address public owner;
+
+    //Modeling Struct
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    //Creating Event
+    event List(string name, uint256 cost, uint256 quantity);
+
+    //modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    //Listing Products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        //Creating Struct
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        //Saving Item Struct
+        items[_id] = item;
+
+        //Emmiting List Event
+        emit List(_name, _cost, _stock);
+    }
+
+    //Buying Products
+
+    //Withdraw Funds
+}
--- test/EtherZon.js
+++ test/EtherZon.js
@@ -1,7 +1,65 @@
 const { expect } = require("chai");
+const { randomBytes } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-describe("EtherZon", () => {});
+// Constants for listing item
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Chothing";
+const IMAGE =
+  "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
+
+describe("EtherZon", () => {
+  let etherZon;
+  let deployer, buyer;
+
+  beforeEach(async () => {
+    //Setup Account
+    [deployer, buyer] = await ethers.getSigners();
+
+    //Deploying Contract
+    const EtherZon = await ethers.getContractFactory("EtherZon");
+    etherZon = await EtherZon.deploy();
+  });
+
+  describe("Deployments", () => {
+    it("Sets the owner", async () => {
+      const owner = await etherZon.owner();
+      expect(owner).to.equal(deployer.address);
+    });
+  });
+
+  describe("Listing", () => {
+    let transacton;
+
+    beforeEach(async () => {
+      transacton = await etherZon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+
+      await transacton.wait();
+    });
+
+    it("Returns Item attributes", async () => {
+      const item = await etherZon.items(1);
+      expect(item.id).to.equal(1);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
+
+    it("Emits List Event", async () => {
+      expect(transacton).to.emit(etherZon, "List");
+    });
+  });
+});
