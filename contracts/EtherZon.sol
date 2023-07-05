4c45f46f9c33dfd8081579e00b6891b4725d9de1
Toshin Prince <toshinprince@gmail.com>
2023-07-05 09:59:25 +0530

    buy Functionality

--- contracts/EtherZon.sol
+++ contracts/EtherZon.sol
@@ -15,9 +15,17 @@ contract EtherZon {
         uint256 stock;
     }
 
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
     mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
 
     //Creating Event
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
     event List(string name, uint256 cost, uint256 quantity);
 
     //Modifier
@@ -54,11 +62,35 @@ contract EtherZon {
         //Saving Item Struct
         items[_id] = item;
 
-        //Emmiting List Event
+        //Emiting List Event
         emit List(_name, _cost, _stock);
     }
 
     //Buying Products
+    function buy(uint256 _id) public payable {
+        //Fetch item
+        Item memory item = items[_id];
+
+        //Require Enough ether to buy
+        require(msg.value >= item.cost);
+
+        //Require item is in stock
+        require(item.stock > 0);
+
+        //Create an Order
+        Order memory order = Order(block.timestamp, item);
+
+        //order count
+        //Adding order for user
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        //Substracting Stock
+        items[_id].stock = item.stock - 1;
+
+        //emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
 
     //Withdraw Funds
 }
